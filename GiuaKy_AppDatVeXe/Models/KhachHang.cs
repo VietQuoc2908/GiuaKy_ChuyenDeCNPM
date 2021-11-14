679708752916d970714f2deb69c41e102d0ad95d
pviet058@gmail.com <pviet058@gmail.com>
2021-11-14 21:45:46 +0700

    Them cac class

---
+++ GiuaKy_AppDatVeXe/Models/KhachHang.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiuaKy_AppDatVeXe.Models
+{
+    class KhachHang
+    {
+        string idKhachHang { get; set; }
+        string hoTen { get; set; }
+        string sdt { get; set; }
+
+        public KhachHang() { }
+
+        public KhachHang(string idKhachHang, string hoTen, string sdt)
+        {
+            this.idKhachHang = idKhachHang;
+            this.hoTen = hoTen;
+            this.sdt = sdt;
+        }
+    }
+}
---
+++ GiuaKy_AppDatVeXe/Models/Ve.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiuaKy_AppDatVeXe.Models
+{
+    class Ve
+    {
+        string idVe { get; set; }
+        string idChuyenDi { get; set; }
+        string idGioKhoiHanh { get; set; }
+        string idXe { get; set; }
+        string soGhe { get; set; }
+        string idKhachHang { get; set; }
+        int giaVe { get; set; }
+    }
+}
